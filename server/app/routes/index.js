c84d040baa5be71714c46d2d08da91430853cc32
Alexandra Polubiec <apolubi@Alexandras-MacBook-Pro.local>
2016-02-13 10:57:35 -0500

    initial commit

---
+++ server/app/routes/index.js
@@ -0,0 +1,30 @@
+'use strict';
+var router = require('express').Router();
+
+router.get('/', function(req, res, next) {
+
+});
+
+router.get('/companies', function(req, res, next) {
+
+});
+
+router.get('/companies/:company', function(req, res, next) {
+
+});
+
+router.get('/companies/:company/review', function(req, res, next) {
+
+});
+
+router.put('/companies/:company', function(req, res, next) {
+
+});
+
+// Make sure this is after all of
+// the registered routes!
+router.use(function (req, res) {
+  res.status(404).end();
+});
+
+module.exports = router;
